eaba67a27ee70d6e99f8631f2c37f5e6307794eb
developer2 <mazzachris.work+developer2@gmail.com>
2022-04-10 20:07:26 -0500

    Initial Git Add

---
+++ src/test/java/mine/mazzanet/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mine.mazzanet.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
